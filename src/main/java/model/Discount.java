b667ca26c5a5eed4a07b7d79edca92f920a77848
HyunJoong Kim <80106238+hjk0761@users.noreply.github.com>
2023-11-09 23:26:54 +0900

    refactor: totalDiscount를 계산하도록 변경

--- src/main/java/model/Discount.java
+++ src/main/java/model/Discount.java
@@ -15,6 +15,7 @@ public class Discount {
     private static final String WEEKDAY_DISCOUNT = "평일 할인";
     private static final String WEEKEND_DISCOUNT = "주말 할인";
     private static final String SPECIAL_DISCOUNT = "특별 할인";
+    private int totalDiscount = 0;
 
     public Map<String, Integer> calculateDiscount(int date, int dessert, int main){
         Map<String, Integer> temp = new HashMap<>();
@@ -29,30 +30,42 @@ public class Discount {
         return temp;
     }
 
+    public int getTotalDiscount(){
+        return totalDiscount;
+    }
+
     private Map<String, Integer> discountD_DAY(int day){
         if (day <= D_DAY){
-            return new HashMap<>(){{put(D_DAY_DISCOUNT, 900 + day * 100);}};
+            int d_day = 900 + day * 100;
+            totalDiscount += d_day;
+            return new HashMap<>(){{put(D_DAY_DISCOUNT, d_day);}};
         }
         return new HashMap<>();
     }
 
     private Map<String, Integer> discountWeekDay(int day, int dessert){
         if (!EventConfig.WEEKEND.contains(day)){
-            return new HashMap<>(){{put(WEEKDAY_DISCOUNT, dessert * WEEKDAY_DISCOUNT_AMOUNT);}};
+            int weekday = dessert * WEEKDAY_DISCOUNT_AMOUNT;
+            totalDiscount += weekday;
+            return new HashMap<>(){{put(WEEKDAY_DISCOUNT, weekday);}};
         }
         return new HashMap<>();
     }
 
     private Map<String, Integer> discountWeekEnd(int day, int main){
         if (EventConfig.WEEKEND.contains(day)){
-            return new HashMap<>(){{put(WEEKEND_DISCOUNT, main * WEEKEND_DISCOUNT_AMOUNT);}};
+            int weekend = main * WEEKEND_DISCOUNT_AMOUNT;
+            totalDiscount += weekend;
+            return new HashMap<>(){{put(WEEKEND_DISCOUNT, weekend);}};
         }
         return new HashMap<>();
     }
 
     private Map<String, Integer> discountSpecial(int day){
         if (EventConfig.SPECIAL.contains(day)){
-            return new HashMap<>(){{put(SPECIAL_DISCOUNT, SPECIAL_DISCOUNT_AMOUNT);}};
+            int special = SPECIAL_DISCOUNT_AMOUNT;
+            totalDiscount += special;
+            return new HashMap<>(){{put(SPECIAL_DISCOUNT, special);}};
         }
         return new HashMap<>();
     }
