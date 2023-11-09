61d409fbba799f49bdf317c31fc7c0cdfe924dc9
HyunJoong Kim <80106238+hjk0761@users.noreply.github.com>
2023-11-09 16:29:35 +0900

    feat: 메뉴 ENUM 객체 구현

---
+++ src/main/java/config/EventConfig.java
@@ -0,0 +1,52 @@
+package config;
+
+import java.util.Arrays;
+
+public class EventConfig {
+    public enum MENU{
+        MUSHROOM_SOUP("양송이수프", "appetizer", 6000),
+        TAPAS("타파스", "appetizer", 5500),
+        CAESAR_SALAD("시저샐러드", "appetizer", 8000),
+        T_BONE_STEAK("티본스테이크", "main", 55000),
+        BARBEQUE_RIBS("바비큐립", "main", 54000),
+        SEAFOOD_PASTA("해산물파스타", "main", 35000),
+        CHRISTMAS_PASTA("크리스마스파스타", "main", 25000),
+        CHOCO_CAKE("초코케이크", "dessert", 15000),
+        ICE_CREAM("아이스크림", "dessert", 5000),
+        ZERO_COKE("제로콜라", "beverage", 3000),
+        RED_WINE("레드와인", "beverage", 60000),
+        CHAMPAGNE("샴페인", "beverage", 25000);
+
+        private final String koreanName;
+        private final String foodType;
+        private final int value;
+
+        MENU(String koreanName, String foodType, int value){
+            this.koreanName = koreanName;
+            this.foodType = foodType;
+            this.value = value;
+        }
+
+        public String getFoodType(){
+            return foodType;
+        }
+
+        public int getValue(){
+            return value;
+        }
+
+        public static MENU valueOfKoreanName(String koreanName) {
+            return Arrays.stream(values())
+                    .filter(value -> value.koreanName == koreanName)
+                    .findAny()
+                    .orElse(null);
+        }
+
+        public static MENU valueOfFoodType(String foodType) {
+            return Arrays.stream(values())
+                    .filter(value -> value.foodType == foodType)
+                    .findAny()
+                    .orElse(null);
+        }
+    }
+}
