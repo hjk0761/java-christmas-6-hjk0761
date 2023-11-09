07851f114425025b33a0e959cdea9a4fd376868b
HyunJoong Kim <80106238+hjk0761@users.noreply.github.com>
2023-11-09 19:18:10 +0900

    refactor: 할인 이벤트 리턴 타입 변경

--- src/main/java/model/Discount.java
+++ src/main/java/model/Discount.java
@@ -2,14 +2,43 @@ package model;
 
 import config.EventConfig;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Discount {
 
-    public int calculateDiscount(int day, int dessert, int main){
-        return discountD_DAY(day) + discountWeekDay(day, dessert) + discountWeedEnd(day, main) + discountSpecial(day);
+    private static final int D_DAY = 25;
+    private static final int WEEKDAY_DISCOUNT_AMOUNT = 2023;
+    private static final int WEEKEND_DISCOUNT_AMOUNT = 2023;
+    private static final int SPECIAL_DISCOUNT_AMOUNT = 1000;
+    private static final String D_DAY_DISCOUNT = "크리스마스 디데이 할인";
+    private static final String WEEKDAY_DISCOUNT = "평일 할인";
+    private static final String WEEKEND_DISCOUNT = "주말 할인";
+    private static final String SPECIAL_DISCOUNT = "특별 할인";
+
+    public Map<String, Integer> calculateDiscount(int day, int dessert, int main){
+        Map<String, Integer> temp = new HashMap<>();
+        int d_day = discountD_DAY(day);
+        if (d_day != 0){
+            temp.put(D_DAY_DISCOUNT, d_day);
+        }
+        int weekday = discountWeekDay(day, dessert);
+        if (weekday != 0){
+            temp.put(WEEKDAY_DISCOUNT, weekday);
+        }
+        int weekend = discountWeedEnd(day, main);
+        if (weekend != 0){
+            temp.put(WEEKEND_DISCOUNT, weekend);
+        }
+        int special = discountSpecial(day);
+        if (special != 0){
+            temp.put(SPECIAL_DISCOUNT, special);
+        }
+        return temp;
     }
 
     private int discountD_DAY(int day){
-        if (day <= 25){
+        if (day <= D_DAY){
             return 900 + day * 100;
         }
         return 0;
@@ -17,21 +46,21 @@ public class Discount {
 
     private int discountWeekDay(int day, int dessert){
         if (!EventConfig.WEEKEND.contains(day)){
-            return dessert * 2023;
+            return dessert * WEEKDAY_DISCOUNT_AMOUNT;
         }
         return 0;
     }
 
     private int discountWeedEnd(int day, int main){
         if (EventConfig.WEEKEND.contains(day)){
-            return main * 2023;
+            return main * WEEKEND_DISCOUNT_AMOUNT;
         }
         return 0;
     }
 
     private int discountSpecial(int day){
         if (EventConfig.SPECIAL.contains(day)){
-            return 1000;
+            return SPECIAL_DISCOUNT_AMOUNT;
         }
         return 0;
     }
